6d2e441d06fb62367dcea3c906d1b95326964838
dsllt <dayanesallet@hotmail.com>
2023-12-23 19:28:28 -0300

    Creates initial form for new account

---
+++ src/app/components/new-account-form/new-account-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-account-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-account-form.component.html',
+  styleUrl: './new-account-form.component.css'
+})
+export class NewAccountFormComponent {
+
+}
