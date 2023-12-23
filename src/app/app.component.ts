5422e9a9f0a3b374ea4997002b70a762cdd07dbe
dsllt <dayanesallet@hotmail.com>
2023-12-23 19:29:29 -0300

    Import and test new components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MainHeaderComponent } from './components/main-header/main-header.component';
+import { MainBodyComponent } from './components/main-body/main-body.component';
+import { MainFooterComponent } from './components/main-footer/main-footer.component';
+import { NewAccountFormComponent } from './components/new-account-form/new-account-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MainHeaderComponent,
+    MainBodyComponent,
+    MainFooterComponent,
+    NewAccountFormComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'unicred_frontend';
