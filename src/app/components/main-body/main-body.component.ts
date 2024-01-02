9292b061bcdbb1edac04df430d0b30da92b6d981
dsllt <dayanesallet@hotmail.com>
2024-01-02 11:03:28 -0300

    Refactor style of home page

--- src/app/components/main-body/main-body.component.ts
+++ src/app/components/main-body/main-body.component.ts
@@ -1,12 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-main-body',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './main-body.component.html',
-  styleUrl: './main-body.component.css'
+  styleUrl: './main-body.component.css',
 })
-export class MainBodyComponent {
-
-}
+export class MainBodyComponent {}
